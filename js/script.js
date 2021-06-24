f75edb3c7f748075eed4aced265f9f873646674a
noemi86-cpu <noemipatermo@gmail.com>
2021-06-24 18:09:33 +0200

    vue-hello

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 //Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 new Vue (
     {
         el: "#app",
         data: {
             text: "Ciao Noemi! Ricorda: il caldo non esiste!",
-            model: "ciaoooo"
+            model: "",
+            img: "https://via.placeholder.com/150",   //img dentro al v-bind
+            classContent: "hidden"
+        }, 
+        methods: {
+            showElement : function(){
+                this.classContent = "show";
+                this.img = "./img/girasole.jpg"
+            } 
         }
+        
     }
 )
 
 
 
-//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
