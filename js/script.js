3d8ca68e7d684d0ed0c90f52067d1408a9fe46e5
noemi86-cpu <noemipatermo@gmail.com>
2021-06-25 11:22:04 +0200

    vue-hello

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 //Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Vue.config.devtools = true;
 new Vue (
     {
         el: "#app",
